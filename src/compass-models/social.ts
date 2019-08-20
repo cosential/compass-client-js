1489541f54665d818ad41f734501f0fa9ac53941
Nathan Williamson <nwilliamson@cosential.com>
2019-08-20 10:19:37 -0500

    add doc changes

--- src/compass-models/social.ts
+++ src/compass-models/social.ts
@@ -1,5 +1,47 @@
 /**
- * Represents a Social Platform.
+ * Represents the Social Platform options.
+ * @example Read a Contact's Socials
+ * ```
+ * client.get<Social[]>('/contacts/5555999/social').then( (res) => {
+ *      if (res.success) {
+ *          res.result.forEach( (index) => { console.log(index.Id); } );
+ *      } else { //something went wrong
+ *          console.log(res.message);
+ *      }
+ * });
+ * ```
+ * @example Add Socials to a contact's Socials
+ * ```
+ * // payload is an array of Division elements to be added
+ * client.post<Social[]>('/contacts/5555999/social', payload).then( (res) => {
+ *      if (res.success) {
+ *          res.result.forEach( (index) => { console.log(index.Id); } );
+ *      } else { //something went wrong
+ *          console.log(res.message);
+ *      }
+ * });
+ * ```
+ * @example Read a Company's Socials
+ * ```
+ * client.get<Social[]>('/companies/5555999/social').then( (res) => {
+ *      if (res.success) {
+ *          res.result.forEach( (index) => { console.log(index.Id); } );
+ *      } else { //something went wrong
+ *          console.log(res.message);
+ *      }
+ * });
+ * ```
+ * @example Add Socials to a Company's Socials
+ * ```
+ * // payload is an array of Division elements to be added
+ * client.post<Social[]>('/companies/5555999/social', payload).then( (res) => {
+ *      if (res.success) {
+ *          res.result.forEach( (index) => { console.log(index.Id); } );
+ *      } else { //something went wrong
+ *          console.log(res.message);
+ *      }
+ * });
+ * ```
  */
 export interface Social {
   Id: number;
