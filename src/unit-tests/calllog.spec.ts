039a8e99349eeb3b73690f6fee3089274d4ebb22
Dawson Turechek <dturechek@cosential.com>
2019-10-25 14:47:09 -0500

    adds tests for call log subitems

--- src/unit-tests/calllog.spec.ts
+++ src/unit-tests/calllog.spec.ts
@@ -1,11 +1,16 @@
 import 'jasmine';
 import * as uuid from 'uuid/v4';
-import { CallLog } from '../compass-models/call-log/call-log';
+import { CallLog, CallLogRole } from '../compass-models/call-log/call-log';
 import { ResponseData } from '../interfaces/response-data';
 import { ClientConfig } from '../service-models/client-config';
 import { Client } from '../services/client';
 import { TestClientConfig as c } from './test-client-config';
 import { MeetingPlan } from '../compass-models/call-log/meeting-plan';
+import { Contact } from '../compass-models/contact/contact';
+import { Company } from '../compass-models/company/company';
+import { CallLogContact } from '../compass-models/call-log/call-log-contact';
+import { Personnel } from '../compass-models/personnel';
+import { CallLogPersonnel } from '../compass-models/call-log/call-log-personnel';
 
 describe('CallLogClient', () => {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
@@ -98,5 +103,85 @@ describe('CallLogClient', () => {
     response = await client.delete < MeetingPlan > (url);
     expect(response.success).withContext('successful delete to call log meeting plan').toBe(true);
     expect(response.result).withContext('null get result').toBeNull();
+  });
+
+  it('Should perform Call log Contact CRUD', async () => {
+    let contactCompany = < Company > {
+      Name: 'UTCompany ' + uuid()
+    };
+    url = '/companies';
+    let contactCompanyCreateRes: ResponseData < Company[] > = await client.post < Company[] > (url, [contactCompany]);
+    expect(contactCompanyCreateRes.success).withContext('successful create ' + url).toBe(true);
+    expect(contactCompanyCreateRes.result).withContext('non-null create result').not.toBeNull();
+    expect(contactCompanyCreateRes.result.length).withContext('correct create result size').toEqual(1);
+    expect(contactCompanyCreateRes.result[0].Name).withContext('correct data in create result').toEqual(contactCompany.Name);
+
+    contactCompany = contactCompanyCreateRes.result[0];
+
+    let contact = < Contact > {
+      CompanyId: contactCompany.CompanyId,
+      FirstName: 'UTContact ' + uuid(),
+      LastName: 'McCoy'
+    };
+    url = '/contacts';
+    let contactCreateRes: ResponseData < Contact[] > = await client.post < Contact[] > (url, [contact]);
+    expect(contactCreateRes.success).withContext('successful create ' + url).toBe(true);
+    expect(contactCreateRes.result).withContext('non-null create result').not.toBeNull();
+    expect(contactCreateRes.result.length).withContext('correct create result size').toEqual(1);
+    expect(contactCreateRes.result[0].FirstName).withContext('correct data in create result').toEqual(contact.FirstName);
+
+    contact = contactCreateRes.result[0];
+
+    url = '/calllogs/' + calllog.id + '/contacts';
+    let payload: CallLogContact = {
+      Contact: contact,
+      role: CallLogRole.Attendee
+    }
+    let addContactRes = await client.post < CallLogContact[] > (url, [payload]);
+    expect(addContactRes.success).withContext('successful add ' + url).toBe(true);
+    expect(addContactRes.result).withContext('non-null add result').not.toBeNull();
+    expect(addContactRes.result.length).withContext('correct add result size').toEqual(1);
+    expect(addContactRes.result[0].Contact.FirstName).toEqual(contact.FirstName);
+
+    let removeContactRes = await client.delete(url);
+    expect(removeContactRes.success).withContext('successful remove').toBe(true);
+
+    url = '/contacts/' + contact.ContactId;
+    let contactDeleteRes = await client.delete(url);
+
+    url = '/companies/' + contactCompany.CompanyId;
+    let companyDeleteRes = await client.delete(url);
+  });
+
+  it('Should perform call log personnel CRUD', async () => {
+    let personnel = < Personnel > {
+      FirstName: 'UTPersonnel ' + uuid(),
+      LastName: 'Young'
+    };
+    url = '/personnel';
+    let personnelCreateRes: ResponseData < Personnel[] > = await client.post < Personnel[] > (url, [personnel]);
+    expect(personnelCreateRes.success).withContext('successful create ' + url).toBe(true);
+    expect(personnelCreateRes.result).withContext('non-null create result').not.toBeNull();
+    expect(personnelCreateRes.result.length).withContext('correct create result size').toEqual(1);
+    expect(personnelCreateRes.result[0].FirstName).withContext('correct data in create result').toEqual(personnel.FirstName);
+
+    personnel = personnelCreateRes.result[0];
+
+    url = '/calllogs/' + calllog.id + '/personnel';
+    const payload: CallLogPersonnel = {
+      Personnel: personnel,
+      role: CallLogRole.Attendee
+    }
+    let personnelAddRes: ResponseData < CallLogPersonnel[] > = await client.post (url, [payload]);
+    expect(personnelAddRes.success).withContext('successful add ' + url).toBe(true);
+    expect(personnelAddRes.result).withContext('non-null add result').not.toBeNull();
+    // Personnel endpoint does not return the personel on post so we do not test
+
+    // Personnel endpoint does not support removing personnel currently so we do not test
+    // let personnelRemoveRes = await client.delete(url);
+    // expect(personnelRemoveRes.success).withContext('Successful remove').toBe(true);
+
+    url = '/personnel/' + personnel.PersonnelId;
+    let personelDeleteRes = await client.delete(url);
   });
 });
